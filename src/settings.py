9ba0928b2551b5e3b03315fa45c76b6e521925ad
Almas Hossen <almashossen028@gmail.com>
2026-08-31 00:55:08 +0600

    Add production security settings

--- src/settings.py
+++ src/settings.py
@@ -189,3 +189,18 @@ SPECTACULAR_SETTINGS = {
         },
     ],
 }
+
+# Production security settings
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_REFERRER_POLICY = "same-origin"
+    X_FRAME_OPTIONS = "DENY"
